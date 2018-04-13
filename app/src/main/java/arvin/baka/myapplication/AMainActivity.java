c8ec12d6bb693d403489bf945df4b9ee31a0c862
bakarvin <arviin.maulana17@gmail.com>
2018-04-13 11:31:06 +0700

    new game

---
+++ app/src/main/java/arvin/baka/myapplication/AMainActivity.java
@@ -0,0 +1,13 @@
+package arvin.baka.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
